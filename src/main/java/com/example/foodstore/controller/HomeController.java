32962c85a8690c0e57280319ec6e8143a18ca8d2
KOS <ngongocthong1211@gmail.com>
2024-10-24 22:31:55 +0700

    Tải trang chủ hệ thống

--- src/main/java/com/example/foodstore/controller/HomeController.java
+++ src/main/java/com/example/foodstore/controller/HomeController.java
@@ -8,9 +8,8 @@ import org.springframework.web.bind.annotation.GetMapping;
 @Controller
 public class HomeController {
 
-    @GetMapping("/")
-    public String index(Model model) {
-
+    @GetMapping({"/", "/index"})
+    public String index() {
         return "web/index";
     }
 }
