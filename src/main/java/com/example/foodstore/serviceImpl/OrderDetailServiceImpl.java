2ae935e7ed5ee86a61449208fdbce40ad74e4b1a
KOS <ngongocthong1211@gmail.com>
2024-12-18 00:02:15 +0700

    Cập nhật triển khai logic cho OrderDetails

--- src/main/java/com/example/foodstore/serviceImpl/OrderDetailServiceImpl.java
+++ src/main/java/com/example/foodstore/serviceImpl/OrderDetailServiceImpl.java
@@ -1,2 +1,23 @@
-package com.example.foodstore.serviceImpl;public class OrderDetailServiceImpl {
+package com.example.foodstore.serviceImpl;
+
+import com.example.foodstore.entity.Order;
+import com.example.foodstore.entity.OrderDetail;
+import com.example.foodstore.repository.OrderDetailRepository;
+import com.example.foodstore.service.OrderDetailService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class OrderDetailServiceImpl implements OrderDetailService {
+
+    @Autowired
+    private OrderDetailRepository orderDetailRepository;
+
+    @Override
+    public List<OrderDetail> findDetailsByOrder(Order order) {
+        return orderDetailRepository.findByOrder(order);
+    }
 }
+
