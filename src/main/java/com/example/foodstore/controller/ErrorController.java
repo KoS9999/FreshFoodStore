f29e1e9447303042196392bef9eb39bc75aa2f95
KOS <ngongocthong1211@gmail.com>
2024-12-18 00:24:35 +0700

    Cập nhật logic xử lý trang error

--- src/main/java/com/example/foodstore/controller/ErrorController.java
+++ src/main/java/com/example/foodstore/controller/ErrorController.java
@@ -5,7 +5,6 @@ import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class ErrorController {
-
     @GetMapping("/error")
     public String error() {
         return "web/404";
