2913a1d0228a942e64639e242873fc27564d8d65
KOS <ngongocthong1211@gmail.com>
2024-12-18 00:19:56 +0700

    Cập nhật logic xử lý hiển thị trang About

--- src/main/java/com/example/foodstore/controller/AboutController.java
+++ src/main/java/com/example/foodstore/controller/AboutController.java
@@ -5,7 +5,6 @@ import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class AboutController {
-
     @GetMapping("/about")
     public String about() {
         return "web/about";
