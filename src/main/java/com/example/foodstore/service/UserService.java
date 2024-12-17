e323ea9685ea5e5ef42bf97c59fbc3bc398029e6
KOS <ngongocthong1211@gmail.com>
2024-12-17 23:51:20 +0700

    Cập nhật interface cho User

--- src/main/java/com/example/foodstore/service/UserService.java
+++ src/main/java/com/example/foodstore/service/UserService.java
@@ -2,19 +2,17 @@ package com.example.foodstore.service;
 
 import com.example.foodstore.entity.User;
 import java.util.List;
+import java.util.Optional;
 
 public interface UserService {
-    List<User> getAllUsers();
-
-    User getUserById(Long id);
-
-    User saveUser(User user);
-
-    void deleteUser(Long id);
-
-    User findByEmail(String email);
 
     List<User> findAll();
+
+    Optional<User> findById(Long id);
+
+    void save(User currentUser);
+
+    User getLoggedInUser();
 
 
 }
