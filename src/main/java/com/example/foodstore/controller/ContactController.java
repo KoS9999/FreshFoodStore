9a2277833a152ade073f5dcb2645bae64b156300
KOS <ngongocthong1211@gmail.com>
2024-12-18 00:20:21 +0700

    Cập nhật logic xử lý hiển thị trang Contact

--- src/main/java/com/example/foodstore/controller/ContactController.java
+++ src/main/java/com/example/foodstore/controller/ContactController.java
@@ -5,7 +5,6 @@ import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class ContactController {
-
     @GetMapping("/contact")
     public String contact() {
         return "web/contact";
