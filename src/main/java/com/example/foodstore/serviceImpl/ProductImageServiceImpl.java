15c6e50b0346c690c86399f792ee8c44319924b8
KOS <ngongocthong1211@gmail.com>
2024-12-18 00:04:20 +0700

    Cập nhật triển khai logic cho ảnh phụ

--- src/main/java/com/example/foodstore/serviceImpl/ProductImageServiceImpl.java
+++ src/main/java/com/example/foodstore/serviceImpl/ProductImageServiceImpl.java
@@ -1,16 +1,32 @@
 package com.example.foodstore.serviceImpl;
 
+import com.example.foodstore.entity.Product;
 import com.example.foodstore.entity.ProductImage;
 import com.example.foodstore.repository.ProductImageRepository;
+import com.example.foodstore.service.ProductImageService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import java.util.List;
+
 @Service
-public class ProductImageServiceImpl {
+public class ProductImageServiceImpl implements ProductImageService {
     @Autowired
     private ProductImageRepository productImageRepository;
 
+    @Override
     public ProductImage save(ProductImage productImage) {
         return productImageRepository.save(productImage);
+    }
+
+    @Override
+    public List<ProductImage> findByProduct(Product product) {
+        return productImageRepository.findByProduct(product);
+    }
+
+
+    @Override
+    public void deleteImageById(Long imageId) {
+        productImageRepository.deleteById(imageId);
     }
 }
