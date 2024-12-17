d4732c497e91e45c775ad6ed4866485fb49d9747
KOS <ngongocthong1211@gmail.com>
2024-12-17 23:47:33 +0700

    Cập nhật interface cho OrderDetail

--- src/main/java/com/example/foodstore/service/OrderDetailService.java
+++ src/main/java/com/example/foodstore/service/OrderDetailService.java
@@ -1,2 +1,11 @@
-package com.example.foodstore.service;public interface OrderDetailService {
+package com.example.foodstore.service;
+
+import com.example.foodstore.entity.Order;
+import com.example.foodstore.entity.OrderDetail;
+
+import java.util.List;
+
+public interface OrderDetailService {
+    List<OrderDetail> findDetailsByOrder(Order order);
 }
+
