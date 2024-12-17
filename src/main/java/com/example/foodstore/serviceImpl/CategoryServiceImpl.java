b8006334cd6e1dc009ad1e3aa6775809d4047b79
KOS <ngongocthong1211@gmail.com>
2024-12-17 23:57:36 +0700

    Cập nhật triển khai logic cho Category

--- src/main/java/com/example/foodstore/serviceImpl/CategoryServiceImpl.java
+++ src/main/java/com/example/foodstore/serviceImpl/CategoryServiceImpl.java
@@ -1,7 +1,9 @@
 package com.example.foodstore.serviceImpl;
 
 import com.example.foodstore.entity.Category;
+import com.example.foodstore.entity.Product;
 import com.example.foodstore.repository.CategoryRepository;
+import com.example.foodstore.repository.ProductRepository;
 import com.example.foodstore.service.CategoryService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
@@ -13,6 +15,9 @@ public class CategoryServiceImpl implements CategoryService {
 
     @Autowired
     private CategoryRepository categoryRepository;
+
+    @Autowired
+    private ProductRepository productRepository;
 
     @Override
     public List<Category> findAll() {
@@ -31,13 +36,21 @@ public class CategoryServiceImpl implements CategoryService {
 
     @Override
     public void update(Long id, Category category) {
-
+        Category existingCategory = categoryRepository.findById(id).orElse(null);
+        if (existingCategory != null) {
+            existingCategory.setCategoryName(category.getCategoryName());
+            existingCategory.setCategoryImage(category.getCategoryImage());
+            categoryRepository.save(existingCategory);
+        }
     }
 
     @Override
     public void delete(Long id) {
-
+        categoryRepository.deleteById(id);
     }
 
+    @Override
+    public List<Product> getAll() {
+        return productRepository.findAll();
+    }
 }
-
