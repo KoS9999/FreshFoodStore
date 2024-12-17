084ef7de9a8398529466f7a4f069d7b9aa8ec172
KOS <ngongocthong1211@gmail.com>
2024-12-17 23:52:14 +0700

    Cập nhật interface cho Cart

--- src/main/java/com/example/foodstore/service/ShoppingCartService.java
+++ src/main/java/com/example/foodstore/service/ShoppingCartService.java
@@ -5,13 +5,12 @@ import com.example.foodstore.entity.CartItem;
 import java.util.Map;
 
 public interface ShoppingCartService {
-    void add(CartItem cartItem); // Thêm sản phẩm vào giỏ hàng
-    void removeProduct(Long productId); // Xóa sản phẩm khỏi giỏ hàng
-    void updateQuantity(Long productId, int quantity); // Cập nhật số lượng sản phẩm trong giỏ hàng
-    Map<Long, CartItem> getCartItems(); // Lấy danh sách sản phẩm trong giỏ hàng
-    int getCount(); // Lấy tổng số lượng sản phẩm trong giỏ hàng
-    double calculateTotal(); // Tính tổng giá trị giỏ hàng
-
+    void add(CartItem cartItem);
+    void removeProduct(Long productId);
+    void updateQuantity(Long productId, int quantity);
+    Map<Long, CartItem> getCartItems();
+    int getCount();
+    double calculateTotal();
     double getAmount();
 
 }
