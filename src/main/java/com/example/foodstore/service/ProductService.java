5f67c19290318d6574b3a3af6dbb7c65bd816df8
KOS <ngongocthong1211@gmail.com>
2024-12-17 23:50:44 +0700

    Cập nhật interface cho Product

--- src/main/java/com/example/foodstore/service/ProductService.java
+++ src/main/java/com/example/foodstore/service/ProductService.java
@@ -1,18 +1,26 @@
 package com.example.foodstore.service;
 
 import com.example.foodstore.entity.Product;
-
 import java.util.List;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 
 public interface ProductService {
     List<Product> findAll();
     Product findById(Long id);
     Product save(Product product);
-
     void delete(Long id);
+    void addProductImage(Product product, String imageUrl);
+    void deleteProductImages(Product product);
+    List<Product> findProductsByCategoryId(Long id);
+    Product getProductById(Long productId);
 
-    void update(Long id, Product product);
+    List<Product> findTop8ByOrderByEnteredDateDesc();
 
-    Product getProductById(Long id);
-    List<Product> getProductsByCategoryId(Long categoryId);
+    List<Product> getTop8BestSellingProducts();
+
+    Product findByProductId(Long id);
+    Page<Product> searchProducts(String keyword, Long categoryId, double minPrice, double maxPrice, Pageable pageable);
+
+
 }
