8ee2aab838dc0d853ddde34d96dbe91229897fb4
KOS <ngongocthong1211@gmail.com>
2024-12-17 23:49:51 +0700

    Cập nhật interface cho ảnh phụ

--- src/main/java/com/example/foodstore/service/ProductImageService.java
+++ src/main/java/com/example/foodstore/service/ProductImageService.java
@@ -1,2 +1,16 @@
-package com.example.foodstore.service;public interface ProductImageService {
+package com.example.foodstore.service;
+
+import com.example.foodstore.entity.Product;
+import com.example.foodstore.entity.ProductImage;
+
+import java.util.List;
+
+public interface ProductImageService {
+    ProductImage save(ProductImage additionalImage);
+
+    List<ProductImage> findByProduct(Product product);
+
+    void deleteImageById(Long imageId);
+
+
 }
