70a56260d664c6a3ca514365e8b1f7d994509c0c
KOS <ngongocthong1211@gmail.com>
2025-04-01 15:33:11 +0700

    Triển khai các chức năng CRUD, tìm kiếm cho mã giảm giá trong service

--- src/main/java/com/example/foodstore/serviceImpl/VoucherServiceImpl.java
+++ src/main/java/com/example/foodstore/serviceImpl/VoucherServiceImpl.java
@@ -1,2 +1,38 @@
-package com.example.foodstore.serviceImpl;public class VoucherServiceImpl {
+package com.example.foodstore.serviceImpl;
+
+import com.example.foodstore.entity.Voucher;
+import com.example.foodstore.repository.VoucherRepository;
+import com.example.foodstore.service.VoucherService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
+
+@Service
+public class VoucherServiceImpl implements VoucherService {
+
+    @Autowired
+    private VoucherRepository voucherRepository;
+
+    @Override
+    public List<Voucher> findAll() {
+        return voucherRepository.findAll();
+    }
+
+    @Override
+    public Optional<Voucher> findById(Long id) {
+        return voucherRepository.findById(id);
+    }
+
+    @Override
+    public Voucher save(Voucher voucher) {
+        return voucherRepository.save(voucher);
+    }
+
+    @Override
+    public void deleteById(Long id) {
+        voucherRepository.deleteById(id);
+    }
 }
+
