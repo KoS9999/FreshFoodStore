78ed21adb2c72bc33e071352e8c7e7857339213d
KOS <ngongocthong1211@gmail.com>
2025-04-01 15:32:20 +0700

    Tạo interface định nghĩa các chức năng xử lý voucher

--- src/main/java/com/example/foodstore/service/VoucherService.java
+++ src/main/java/com/example/foodstore/service/VoucherService.java
@@ -1,2 +1,13 @@
-package com.example.foodstore.service;public interface VoucherService {
+package com.example.foodstore.service;
+
+import com.example.foodstore.entity.Voucher;
+
+import java.util.List;
+import java.util.Optional;
+
+public interface VoucherService {
+    List<Voucher> findAll();
+    Optional<Voucher> findById(Long id);
+    Voucher save(Voucher voucher);
+    void deleteById(Long id);
 }
