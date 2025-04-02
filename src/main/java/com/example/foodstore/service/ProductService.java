a8f8ce559076b3bbb3d5c21d218ccf9932aee7b0
KOS <ngongocthong1211@gmail.com>
2025-04-02 15:13:48 +0700

    Thêm phương thức getProductsBySeasonMonth để lấy danh sách sản phẩm theo tháng mùa trong ProductService

--- src/main/java/com/example/foodstore/service/ProductService.java
+++ src/main/java/com/example/foodstore/service/ProductService.java
@@ -2,6 +2,8 @@ package com.example.foodstore.service;
 
 import com.example.foodstore.entity.Product;
 import java.util.List;
+import java.util.Set;
+
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 
@@ -14,13 +16,13 @@ public interface ProductService {
     void deleteProductImages(Product product);
     List<Product> findProductsByCategoryId(Long id);
     Product getProductById(Long productId);
-
     List<Product> findTop8ByOrderByEnteredDateDesc();
-
     List<Product> getTop8BestSellingProducts();
-
     Product findByProductId(Long id);
     Page<Product> searchProducts(String keyword, Long categoryId, double minPrice, double maxPrice, Pageable pageable);
-
+    List<Product> getAllProducts();
+    List<Product> getProductsByIds(List<Long> ids);
+    List<Product> getProductsBySeasonMonth(int month);
+    List<Product> findProductsByIds(List<Long> productIds);
 
 }
