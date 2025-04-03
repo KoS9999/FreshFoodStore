9201686d9afb060e3f3bbf28408cdd2beb43229c
KOS <ngongocthong1211@gmail.com>
2025-04-03 16:21:15 +0700

    Triển khai ViewLogService, tự động xóa lịch sử xem cũ mỗi ngày

--- src/main/java/com/example/foodstore/serviceImpl/ViewLogServiceImpl.java
+++ src/main/java/com/example/foodstore/serviceImpl/ViewLogServiceImpl.java
@@ -1,2 +1,27 @@
-package com.example.foodstore.serviceImpl;public class ViewLogServiceImpl {
+package com.example.foodstore.serviceImpl;
+
+import com.example.foodstore.repository.ViewLogRepository;
+import com.example.foodstore.service.ViewLogService;
+import org.springframework.scheduling.annotation.Scheduled;
+import org.springframework.stereotype.Service;
+
+import java.util.Calendar;
+import java.util.Date;
+
+@Service
+public class ViewLogServiceImpl implements ViewLogService{
+    private final ViewLogRepository viewLogRepository;
+
+    public ViewLogServiceImpl(ViewLogRepository viewLogRepository) {
+        this.viewLogRepository = viewLogRepository;
+    }
+
+    @Override
+    @Scheduled(cron = "0 0 1 * * ?")
+    public void cleanOldViewLogs() {
+        Calendar cal = Calendar.getInstance();
+        cal.add(Calendar.DAY_OF_YEAR, -30);
+        Date cutoffDate = cal.getTime();
+        viewLogRepository.deleteByViewTimeBefore(cutoffDate);
+    }
 }
