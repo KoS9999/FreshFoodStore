68c89e361a3b06b7d0845460aed641f8fa5207d9
KOS <ngongocthong1211@gmail.com>
2025-04-09 21:11:53 +0700

    Thêm chức năng gửi thông báo đơn hàng và đánh giá mới khi admin truy cập trang dashboard

--- src/main/java/com/example/foodstore/controlleradmin/AdminController.java
+++ src/main/java/com/example/foodstore/controlleradmin/AdminController.java
@@ -1,5 +1,7 @@
 package com.example.foodstore.controlleradmin;
 
+import com.example.foodstore.service.NotificationService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -8,9 +10,16 @@ import org.springframework.web.bind.annotation.RequestMapping;
 @RequestMapping("/admin")
 public class AdminController {
 
+    private final NotificationService notificationService;
+    @Autowired
+    public AdminController(NotificationService notificationService) {
+        this.notificationService = notificationService;
+    }
+
     @GetMapping("/dashboard")
     public String adminDashboard() {
+        notificationService.sendNewOrderNotification("/topic/admin", "Có đơn hàng mới từ khách hàng!");
+        notificationService.sendNewReviewNotification("/topic/admin", "Có đánh giá mới từ khách hàng!");
         return "admin/dashboard";
     }
 }
-
