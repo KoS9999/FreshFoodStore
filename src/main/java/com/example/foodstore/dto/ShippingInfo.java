f8df491cf4749f464361bdac27bfbdce10971dc2
KOS <ngongocthong1211@gmail.com>
2025-05-09 22:01:56 +0700

    thêm DTO chứa thông tin vận chuyển

--- src/main/java/com/example/foodstore/dto/ShippingInfo.java
+++ src/main/java/com/example/foodstore/dto/ShippingInfo.java
@@ -1,2 +1,10 @@
-package com.example.foodstore.dto;public class ShippingInfo {
+package com.example.foodstore.dto;
+
+import lombok.Data;
+
+@Data
+public class ShippingInfo {
+    private String durationText;
+    private double distanceKm;
+    private double shippingCost;
 }
