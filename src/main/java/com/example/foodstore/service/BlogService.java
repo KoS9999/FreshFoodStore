cbf617a84a3983bb1b5733db033bd7b6c75650d2
KOS <ngongocthong1211@gmail.com>
2025-06-09 16:48:22 +0700

    Thêm BlogService interface với các phương thức lấy bài viết theo danh mục và phân trang

--- src/main/java/com/example/foodstore/service/BlogService.java
+++ src/main/java/com/example/foodstore/service/BlogService.java
@@ -3,13 +3,21 @@ package com.example.foodstore.service;
 import java.util.List;
 import java.util.Optional;
 import com.example.foodstore.entity.Blog;
+import com.example.foodstore.entity.BlogCategory;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.web.multipart.MultipartFile;
 
 public interface BlogService {
-    Blog createBlog(Blog blog, MultipartFile imageFile);
-    Blog updateBlog(Long id, Blog blog, MultipartFile imageFile);
+    void createBlog(Blog blog, MultipartFile imageFile);
+    void updateBlog(Long id, Blog blog, MultipartFile imageFile);
     void deleteBlog(Long id);
     Optional<Blog> getBlogById(Long id);
     Optional<Blog> getBlogBySlug(String slug);
     List<Blog> getAllBlogs();
+    List<Blog> getLatestBlogs();
+    List<Blog> findByCategory(BlogCategory category);
+    Page<Blog> getAllBlogsPage(Pageable pageable);
+    Page<Blog> findByCategoryPage(BlogCategory category, Pageable pageable);
+
 }
