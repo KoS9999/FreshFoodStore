8111cf17c0b1cfdc45eb474aa7e6751472fa4e97
KOS <ngongocthong1211@gmail.com>
2025-06-09 16:37:17 +0700

    Thêm BlogRepository với các phương thức tìm kiếm bài viết theo danh mục và sắp xếp

--- src/main/java/com/example/foodstore/repository/BlogRepository.java
+++ src/main/java/com/example/foodstore/repository/BlogRepository.java
@@ -1,6 +1,9 @@
 package com.example.foodstore.repository;
 
 import com.example.foodstore.entity.Blog;
+import com.example.foodstore.entity.BlogCategory;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.List;
@@ -8,7 +11,9 @@ import java.util.Optional;
 
 public interface BlogRepository extends JpaRepository<Blog, Long> {
     Optional<Blog> findBySlug(String slug);
-
     List<Blog> findByMarkdownContentContainingIgnoreCase(String keyword);
+    List<Blog> findByCategory(BlogCategory category);
+    List<Blog> findAllByOrderByCreatedAtDesc();
+    Page<Blog> findByCategory(BlogCategory category, Pageable pageable);
 
 }
