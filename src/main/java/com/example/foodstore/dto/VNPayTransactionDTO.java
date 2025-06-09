53b486ff822e0ddd9c69c34b7cda1077ccb287c0
KOS <ngongocthong1211@gmail.com>
2025-06-09 16:36:07 +0700

    Thêm DTO VNPayTransactionDTO để xử lý giao dịch VNPay

--- src/main/java/com/example/foodstore/dto/VNPayTransactionDTO.java
+++ src/main/java/com/example/foodstore/dto/VNPayTransactionDTO.java
@@ -1,2 +1,13 @@
-package com.example.foodstore.dto;public class VNPayTransactionDTO {
+package com.example.foodstore.dto;
+
+import lombok.Data;
+
+@Data
+public class VNPayTransactionDTO {
+    private String txnRef;
+    private String appUserEmail;
+    private String embedData;
+    private String items;
+    private double amount;
+    private String createdAt; // yyyyMMddHHmmss format
 }
