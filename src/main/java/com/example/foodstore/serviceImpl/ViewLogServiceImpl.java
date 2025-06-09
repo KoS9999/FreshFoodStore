97ee19fca993536a6563317d7b26e3495af0905f
KOS <ngongocthong1211@gmail.com>
2025-06-09 17:01:02 +0700

    Cập nhật hiển thị lịch sử xem sản phẩm với chức năng lấy sản phẩm gần đây

--- src/main/java/com/example/foodstore/serviceImpl/ViewLogServiceImpl.java
+++ src/main/java/com/example/foodstore/serviceImpl/ViewLogServiceImpl.java
@@ -1,5 +1,7 @@
 package com.example.foodstore.serviceImpl;
 
+import com.example.foodstore.entity.Product;
+import com.example.foodstore.entity.ViewLog;
 import com.example.foodstore.repository.ViewLogRepository;
 import com.example.foodstore.service.ViewLogService;
 import org.springframework.scheduling.annotation.Scheduled;
@@ -7,6 +9,8 @@ import org.springframework.stereotype.Service;
 
 import java.util.Calendar;
 import java.util.Date;
+import java.util.List;
+import java.util.stream.Collectors;
 
 @Service
 public class ViewLogServiceImpl implements ViewLogService{
@@ -23,5 +27,28 @@ public class ViewLogServiceImpl implements ViewLogService{
         cal.add(Calendar.DAY_OF_YEAR, -30);
         Date cutoffDate = cal.getTime();
         viewLogRepository.deleteByViewTimeBefore(cutoffDate);
+    }
+
+    @Override
+    public List<Product> getRecentProducts(Long userId) {
+        List<ViewLog> viewLogs = viewLogRepository.findTop11ByUserUserIdOrderByViewTimeDesc(userId);
+        System.out.println("Fetched " + viewLogs.size() + " ViewLogs for userId: " + userId);
+        // Chuyển đổi sang danh sách Product, loại bỏ trùng lặp dựa trên productId
+        List<Product> products = viewLogs.stream()
+                .map(ViewLog::getProduct)
+                .distinct()
+                .limit(11)
+                .collect(Collectors.toList());
+
+        for (Product product : products) {
+            System.out.println("Product ID: " + product.getProductId() +
+                    ", Product Name: " + product.getProductName());
+        }
+
+        return products;
+    }
+    @Override
+    public void save(ViewLog viewLog) {
+        viewLogRepository.save(viewLog);
     }
 }
