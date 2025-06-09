dee8f90cf080dbff94eff93f55a9d1f072580e7e
KOS <ngongocthong1211@gmail.com>
2025-06-09 16:45:39 +0700

    Thêm ViewLogRepository với truy vấn lấy lịch sử xem sản phẩm của người dùng

--- src/main/java/com/example/foodstore/repository/ViewLogRepository.java
+++ src/main/java/com/example/foodstore/repository/ViewLogRepository.java
@@ -2,10 +2,13 @@ package com.example.foodstore.repository;
 
 import com.example.foodstore.entity.ViewLog;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 import java.util.Date;
+import java.util.List;
 
 public interface ViewLogRepository extends JpaRepository<ViewLog, Long> {
     void deleteByViewTimeBefore(Date cutoffDate);
-
-}
+    @Query("SELECT vl FROM ViewLog vl JOIN FETCH vl.product WHERE vl.user.userId = :userId ORDER BY vl.viewTime DESC")
+    List<ViewLog> findTop11ByUserUserIdOrderByViewTimeDesc(@Param("userId") Long userId);}
